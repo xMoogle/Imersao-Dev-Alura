5058146104149cf4157cc44d4abb0f9eb4e7ecbe
xMoogle <32349043+xMoogle@users.noreply.github.com>
2022-09-10 03:34:36 -0300

    Add files via upload

--- Aula-2/CasaDeCambio/casaDeCambio.js
+++ Aula-2/CasaDeCambio/casaDeCambio.js
@@ -1,15 +1,23 @@
 console.log("Programa iniciado...rodando");
-function Converter() {
+function Converter1() {
   var a = document.getElementById("valor");
   var valor = a.value;
-  var valorEmBitCoin = parseFloat(valor / 18859.43);
-  var valorEmReal = parseFloat(valor * 5);
-  var elementoValorConvertido = document.getElementById("valorConvertido");
-  console.log(elementoValorConvertido);
-  var elementoValorEmBitCoin = document.getElementById("valorEmBitCoin");
-  console.log(elementoValorEmBitCoin);
-  elementoValorConvertido.innerHTML = "O valor em reais é: " + valorEmReal;
-  elementoValorEmBitCoin.innerHTML = "O valor em BitCoin é: " + valorEmBitCoin;
+  var valorEmBananas = parseFloat(valor / 18859.43);
+  var valorEmRupees = parseFloat(valor * 5);
+  var valorEmMoedas = parseFloat(valor * 4);
+  var valorEmAneis = parseFloat(valor * 4);
+  var elementoValorEmRupees = document.getElementById("valorEmRupees");
+  console.log(elementoValorEmRupees);
+  var elementoValorConvertidoB = document.getElementById("valorConvertidoB");
+  console.log(elementoValorConvertidoB);
+  var elementoValorConvertidoC = document.getElementById("valorConvertidoC");
+  console.log(elementoValorEmRupees);
+  var elementoValorConvertidoR = document.getElementById("valorConvertidoR");
+  console.log(elementoValorConvertidoB);
+  elementoValorEmRupees.innerHTML = " O valor em rupees é: " + valorEmRupees;
+  elementoValorConvertidoB.innerHTML =
+    "O valor em Bananas é: " + valorEmBananas;
+  elementoValorConvertidoC.innerHTML = " O valor em rupees é: " + valorEmMoedas;
+  elementoValorConvertidoR.innerHTML = "O valor em Bananas é: " + valorEmAneis;
   console.log("clicou");
-  console.log(valorEmReal);
 }
