c0edcb2d3e7b8e7d1fd02ea0ccf9c8fc6f989aaa
xMoogle <32349043+xMoogle@users.noreply.github.com>
2022-09-11 16:51:42 -0300

    Add files via upload

--- Aula-2/CasaDeCambio/casaDeCambio.js
+++ Aula-2/CasaDeCambio/casaDeCambio.js
@@ -1,11 +1,25 @@
 console.log("Programa iniciado...rodando");
-function Converter1() {
+function Converter(n) {
   var a = document.getElementById("valor");
-  var valor = a.value;
-  var valorEmBananas = parseFloat(valor / 18859.43);
-  var valorEmRupees = parseFloat(valor * 5);
+  switch (n) {
+    case 1:
+      var valor = a.value;
+      break;
+    case 2:
+      var valor = a.value / 50;
+      break;
+    case 3:
+      var valor = a.value / 4;
+      break;
+    case 4:
+      var valor = a.value / 10;
+      break;
+  }
+
+  var valorEmBananas = parseFloat(valor * 50);
+  var valorEmRupees = parseFloat(valor);
   var valorEmMoedas = parseFloat(valor * 4);
-  var valorEmAneis = parseFloat(valor * 4);
+  var valorEmAneis = parseFloat(valor * 10);
   var elementoValorEmRupees = document.getElementById("valorEmRupees");
   console.log(elementoValorEmRupees);
   var elementoValorConvertidoB = document.getElementById("valorConvertidoB");
@@ -17,7 +31,7 @@ function Converter1() {
   elementoValorEmRupees.innerHTML = " O valor em rupees é: " + valorEmRupees;
   elementoValorConvertidoB.innerHTML =
     "O valor em Bananas é: " + valorEmBananas;
-  elementoValorConvertidoC.innerHTML = " O valor em rupees é: " + valorEmMoedas;
-  elementoValorConvertidoR.innerHTML = "O valor em Bananas é: " + valorEmAneis;
+  elementoValorConvertidoC.innerHTML = " O valor em moedas é: " + valorEmMoedas;
+  elementoValorConvertidoR.innerHTML = "O valor em anéis é: " + valorEmAneis;
   console.log("clicou");
 }
